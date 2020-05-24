f336c18a205c53baba33f74d6d3b455c25a18ae4
Puneet Pugalia <puneet.pugalia@gmail.com>
2020-05-24 13:57:59 +0530

    Reading slider value, starting new round, functioning target label using IBOutlets

--- ElephantsEye/ViewController.swift
+++ ElephantsEye/ViewController.swift
@@ -9,21 +9,49 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var currentValue:Int = 0
+    var targetValue:Int = 0
+    
+    @IBOutlet weak var slider:UISlider!
+    @IBOutlet weak var targetLabel:UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        currentValue = Int(slider.value.rounded())
+        startNewRound()
     }
 
     @IBAction func showAlert() {
         
-        let alert = UIAlertController(title: "Hello, World!", message: "This is my first app!", preferredStyle: .alert)
+        let message = "The value of the slider is \(currentValue)" +
+        "\nThe target value is \(targetValue)"
+        
+        let alert = UIAlertController(title: "Hello, World!", message: message, preferredStyle: .alert)
         
         let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
         
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
+        
+        startNewRound()
+    }
+    
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        currentValue = Int(slider.value.rounded())
+    }
+    
+    func startNewRound() {
+        targetValue = Int.random(in: 1...100)
+        currentValue = 50
+        slider.value = Float(currentValue)
+        updateLabels()
+    }
+    
+    func updateLabels() {
+        targetLabel.text = String(targetValue)
     }
 
 }
